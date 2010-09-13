05ef2b650ccdd72a49d8654a0c346031cf5627e7
Dylan Egan <me@dylanegan.com>
2010-09-14 08:49:40 +1000

    Show some text initially. Don't check weather if outside of time range.

--- public/javascripts/isitciderweather.js
+++ public/javascripts/isitciderweather.js
@@ -1,17 +1,9 @@
 function success(position) {
-  var today = new Date();
-  var hour = today.getHours();
   var geonames = new GeoNames();
 
   geonames.findNearByWeather(position.coords.latitude, position.coords.longitude, function(data) {
     if (data) {
-      if (hour < 9) {
-        $('#outcome').html("it's before 9, ciroc?");
-        $('body').addClass('ciroc');
-      } else if (hour >= 9 && hour < 11) {
-        $('#outcome').html("check in at 11, liver break!");
-        $('body').addClass('liver');
-      } else if (parseInt(data.temperature) >= 19) {
+      if (parseInt(data.temperature) >= 19) {
         $('#outcome').html("sure, i've got mine in hand");
         $('body').addClass('cider');
       } else {
@@ -32,7 +24,18 @@ function error(message) {
   $('body').addClass('error');
 } 
 
-if (navigator.geolocation) {
+var today = new Date(2010, 09, 14, 08, 10);
+var hour = today.getHours();
+
+if (hour < 9) {
+  $('#outcome').html("it's before 9, ciroc?");
+  $('#status').html("(or sleep)");
+  $('body').addClass('ciroc');
+} else if (hour >= 9 && hour < 11) {
+  $('#outcome').html("check in at 11, liver break!");
+  $('#status').html("drink responsibly&#8482;");
+  $('body').addClass('liver');
+} else if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(success, error);
 } else {
   error('not supported');
