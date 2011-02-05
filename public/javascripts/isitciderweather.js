845a7a96c9c917db8ecf171b8ce7fba8dde481ed
Dylan Egan <me@dylanegan.com>
2011-02-04 17:56:13 -0800

    Fucking Oath.

--- public/javascripts/isitciderweather.js
+++ public/javascripts/isitciderweather.js
@@ -4,7 +4,7 @@ function success(position) {
   geonames.findNearByWeather(position.coords.latitude, position.coords.longitude, function(data) {
     if (data) {
       if (parseInt(data.temperature) >= 30) {
-        $('#outcome').html("fuck oath! you're an idiot otherwise");
+        $('#outcome').html("fucking oath! you're an idiot otherwise");
         $('body').addClass('cider');
       } else if (parseInt(data.temperature) >= 19) {
         $('#outcome').html("sure, i've got mine in hand");
